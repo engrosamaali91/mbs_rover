e170ccf4ed7cf034c30ba812e7159693c111fc77
administrator <--global>
2024-11-06 16:21:36 +0100

    files commit

---
+++ src/rover_description/launch/bringup.launch.py
@@ -0,0 +1,54 @@
+
+from launch import LaunchDescription
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [FindPackageShare("rover_description"), "xacro", "robot.xacro"]
+            ),
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
+
+    # Launch Robot State Publisher
+    node_robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        # namespace="rover",
+        output="screen",
+        parameters=[robot_description],
+    )
+
+    # Launch Joint State Publisher GUI
+    node_joint_state_publisher_gui = Node(
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui",
+        name="joint_state_publisher_gui",
+        # namespace="rover",
+        output="screen",
+    )
+
+    # Launch RViz
+    node_rviz = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        # namespace="rover",
+        output="screen",
+        arguments=["-d", PathJoinSubstitution([FindPackageShare("rover_viz"), "rviz", "model.rviz"])],
+    )
+
+    ld = LaunchDescription()
+
+    ld.add_action(node_robot_state_publisher)
+    ld.add_action(node_joint_state_publisher_gui)
+    ld.add_action(node_rviz)
+
+    return ld
---
+++ src/rover_viz/launch/view_robot.launch.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration,PathJoinSubstitution
+from launch.actions import DeclareLaunchArgument, GroupAction
+from launch_ros.actions import Node, PushRosNamespace
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+
+    ns = LaunchConfiguration('namespace')
+    arg_namespace = DeclareLaunchArgument(
+        'namespace',
+        default_value='',
+        description='robot namespace'
+    )
+
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare("rover_viz"), "rviz", "robot.rviz"]
+    )
+
+    node_rviz = GroupAction([
+        PushRosNamespace(ns),
+        Node(package='rviz2',
+             executable='rviz2',
+             name='rviz2',
+             arguments=['-d', rviz_config_file],
+             remappings=[
+                ('/tf', 'tf'),
+                ('/tf_static', 'tf_static')
+             ],
+             output='screen')
+    ])
+
+    ld = LaunchDescription()
+    ld.add_action(arg_namespace)
+    ld.add_action(node_rviz)
+
+    return ld
